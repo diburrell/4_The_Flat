f8ca261b362014049cd3c1292d9d93661ff75326
EmilyGMcDonald <mcdonald.emily@live.co.uk>
2014-03-12 18:58:20 +0000

    Account buttons added
    Four dummy buttons added to the Account screen.

--- 4TheFlatProject/src/com/FourTheFlat/activities/AccountActivity.java
+++ 4TheFlatProject/src/com/FourTheFlat/activities/AccountActivity.java
@@ -1,31 +1,53 @@
 package com.FourTheFlat.activities;
 
-import java.util.Collections;
-
-import com.FourTheFlat.*;
+import com.FourTheFlat.R;
 
 import android.app.Activity;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.text.Html;
-import android.util.Log;
-import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
 
 /*
  * Creates the rocket activity tab. Called initially AgileProjectActivity.
  */
 public class AccountActivity extends Activity
 {
-		/** Called when the activity is first created. */
+	/** Called when the activity is first created. */
 	
-		ConnectionManager connectionManager;
+	TableLayout table;
+	
+	Button accountInfo;
+	Button modifyFlatDetails;
+	Button changePassword;
+	Button logout;
 
-		@Override
-		public void onCreate(Bundle savedInstanceState)
-		{
-			super.onCreate(savedInstanceState);	
-		}	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);	
+		setContentView(R.layout.account);
+		accountDisplay(this);
+	}	
+	
+	public void accountDisplay(Activity contextActivity)
+	{		
+		table = (TableLayout) contextActivity.findViewById(R.id.tableLayout1);
+		
+		accountInfo = new Button(contextActivity);
+		accountInfo.setText("Account Information");
+		table.addView(accountInfo);		
+		
+		modifyFlatDetails = new Button(contextActivity);
+		modifyFlatDetails.setText("Modify Flat Details");
+		table.addView(modifyFlatDetails);
+		
+		changePassword = new Button(contextActivity);
+		changePassword.setText("Change Password");
+		table.addView(changePassword);
+		
+		logout = new Button(contextActivity);
+		logout.setText("Logout");
+		table.addView(logout);		
 	}
+}
