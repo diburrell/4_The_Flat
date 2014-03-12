1d39b60cd5392f37a0b8ad8cde7a7529e8e48586
alexandergrant1 <sandygrant1@gmail.com>
2014-03-12 21:00:06 +0000

    basic functionality of shopping list and products page added.

--- 4TheFlatProject/src/com/FourTheFlat/activities/ProductsActivity.java
+++ 4TheFlatProject/src/com/FourTheFlat/activities/ProductsActivity.java
@@ -1,43 +1,136 @@
 package com.FourTheFlat.activities;
 
-import java.util.Collections;
-
 import com.FourTheFlat.*;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ProductsActivity extends Activity
-{
-		/** Called when the activity is first created. */
+public class ProductsActivity extends Activity implements View.OnClickListener {
+	/** Called when the activity is first created. */
 
-		
-		@Override
-		public void onCreate(Bundle savedInstanceState)
-		{
-			super.onCreate(savedInstanceState);
+	TableLayout buttonHolder;
+
+	/* Create Error Messages */
+	TableRow errorRow;
+	TextView error1;
+
+	Button moreProducts;
+	boolean byValue;
+
+	TableLayout list;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.shoppinglist);
+
+		moreProducts = new Button(this);
+		moreProducts.setText("Add more products!");
+
+		productTable(this);
+	}
+
+	public void productTable(Activity contextActivity) {
+
+		buttonHolder = (TableLayout) contextActivity
+				.findViewById(R.id.tableLayout1);
+		list = (TableLayout) contextActivity.findViewById(R.id.tableLayout2);
+
+		int productCount = 100;
+
+		moreProducts.setOnClickListener(this);
+		buttonHolder.addView(moreProducts);
+
+		TableRow[] rowProduct = new TableRow[productCount];
+		TextView[] productName = new TextView[productCount];
+
+		// FILL THE LIST WITH PRODUCTS ON THE LIST!!!!!!
+		for (int i = 0; i < productCount - 1; i++) {
+
+			rowProduct[i] = new TableRow(contextActivity);
+			productName[i] = new TextView(contextActivity);
+
+			productName[i].setText("Eggs");
+			productName[i].setTypeface(Typeface.DEFAULT, Typeface.BOLD);
+			productName[i].setTextColor(Color.rgb(255, 255, 255));
+			productName[i].setTextSize(25f);
+			productName[i].setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+
+			rowProduct[i].addView(productName[i]);
+			rowProduct[i].setOnClickListener(this);
+
+			list.addView(rowProduct[i]);
+		}
+
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause(); // Always call the superclass method first
+		Log.w("Pause", "Activity Paused");
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume(); // Always call the superclass method first
+		Log.w("Resume", "Activity Resumed");
+	}
+
+	@Override
+	public void onClick(View v)
+	{
 			
-		}
-		
-	
-		
-		@Override
-		public void onPause()
+		if (v instanceof Button)
 		{
-		    super.onPause();  // Always call the superclass method first
-		    Log.w("Pause", "Activity Paused");
+			Toast.makeText(this, "TO DO: SHOW MORE PRODUCTS!", Toast.LENGTH_SHORT).show();
 		}
-		
-		@Override
-		public void onResume() {
-		    super.onResume();  // Always call the superclass method first
-		    Log.w("Resume", "Activity Resumed");
+		else if (v instanceof TableRow)
+		{
+			TableRow tR = (TableRow)v;
+			TextView child = (TextView) tR.getChildAt(0); 
+			
+			
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+ 
+			// set title
+			alertDialogBuilder.setTitle("Do you want to add "+child.getText().toString()+" product to the shopping list?");
+ 
+			// set dialog message
+			alertDialogBuilder
+				.setCancelable(false)
+				.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int id) {
+						
+						///POSITIVE INPUT!
+						
+						Toast.makeText(ProductsActivity.this, "TO DO: ADD ITEM TO SHOPPING LIST!", Toast.LENGTH_LONG).show();
+					}
+				  })
+				.setNegativeButton("No",new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int id) {
+						// if this button is clicked, just close
+						// the dialog box and do nothing
+						dialog.cancel();
+					}
+				});
+ 
+				// create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+ 
+				// show it
+				alertDialog.show();
 		}
 	}
+}
--- 4TheFlatProject/src/com/FourTheFlat/activities/ShoppingListActivity.java
+++ 4TheFlatProject/src/com/FourTheFlat/activities/ShoppingListActivity.java
@@ -1,79 +1,68 @@
 package com.FourTheFlat.activities;
 
-import java.io.IOException;
-import java.text.NumberFormat;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Locale;
-
-import org.json.JSONException;
-
 import com.FourTheFlat.*;
-import com.FourTheFlat.R;
 import com.FourTheFlat.activities.ShoppingListActivity;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ShoppingListActivity extends Activity implements View.OnClickListener {
+public class ShoppingListActivity extends Activity implements
+		View.OnClickListener {
 
-	
+	TableLayout buttonHolder;
+
 	/* Create Error Messages */
-	TableLayout table;
 	TableRow errorRow;
 	TextView error1;
-	TableRow.LayoutParams params;
 
 	Button startShop;
 	boolean byValue;
-	
+
+	TableLayout list;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Alarm.startRepeatingTimer(getApplicationContext());
 
 		setContentView(R.layout.shoppinglist);
-		
+
 		startShop = new Button(this);
-		
-		summaryTable(this);
+		startShop.setText("Find a Shop!");
+
+		listTable(this);
 	}
 
-	public void summaryTable(Activity contextActivity) {
+	public void listTable(Activity contextActivity) {
 
-		table = (TableLayout) contextActivity.findViewById(R.id.tableLayout1); // Find	
+		buttonHolder = (TableLayout) contextActivity
+				.findViewById(R.id.tableLayout1);
+		list = (TableLayout) contextActivity.findViewById(R.id.tableLayout2);
 
-		int productCount = 4;
-		
-		TableRow[] rowProduct = new TableRow[productCount];
-	 
-		TextView[] productName = new TextView[productCount];
-		
-				
+		int productCount = 100;
+
 		startShop.setOnClickListener(this);
-		table.addView(startShop);
-		
-		 
-			for (int i = 0; i < 4 ; i++) {
-					
+		buttonHolder.addView(startShop);
+
+		TableRow[] rowProduct = new TableRow[productCount];
+		TextView[] productName = new TextView[productCount];
+
+		// FILL THE LIST WITH PRODUCTS ON THE LIST!!!!!!
+		for (int i = 0; i < productCount - 1; i++) {
+
 			rowProduct[i] = new TableRow(contextActivity);
 			productName[i] = new TextView(contextActivity);
-	
+
 			productName[i].setText("Eggs");
 			productName[i].setTypeface(Typeface.DEFAULT, Typeface.BOLD);
 			productName[i].setTextColor(Color.rgb(255, 255, 255));
@@ -82,55 +71,26 @@ public class ShoppingListActivity extends Activity implements View.OnClickListen
 
 			rowProduct[i].addView(productName[i]);
 
-			table.addView(rowProduct[i]);
+			list.addView(rowProduct[i]);
 		}
-		
+
 	}
 
-	public void update()
-	{
-		table = (TableLayout) this.findViewById(R.id.tableLayout1);
-
-		errorRow = new TableRow(this);
-		error1 = new TextView(this);
-		params = new TableRow.LayoutParams();
-		params.span = 4;
-
-	
-	}
-
-	
 	@Override
-	public void onClick(View v)
-	{
-		if (v instanceof Button)
-		{
-			if (byValue)
-			{
-				startShop.setText("321...");
-				byValue = false;
-			}
-			else
-			{
-				startShop.setText("ABC...");
-				byValue = true;
-			}
-		}
+	public void onClick(View v) {
+		Toast.makeText(this, "TO DO: SHOW A SHOP!", Toast.LENGTH_SHORT).show();
 	}
-	
-	
+
 	@Override
-	public void onPause()
-	{
-	    super.onPause();  // Always call the superclass method first
-	    Log.w("Pause", "Activity Paused");
+	public void onPause() {
+		super.onPause(); // Always call the superclass method first
+		Log.w("Pause", "Activity Paused");
 	}
-	
+
 	@Override
-	public void onResume()
-	{
-	    super.onResume();  // Always call the superclass method first
-	    
-	    Log.w("Resume", "Activity Resumed");
+	public void onResume() {
+		super.onResume(); // Always call the superclass method first
+
+		Log.w("Resume", "Activity Resumed");
 	}
 }
