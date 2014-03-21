77b38d5c4581039ca4d6a692aa867fc7dd8c434a
Alexander Grant <alexander.grant55@gmail.com>
2014-03-21 17:15:01 +0000

    Start of change password activity

--- 4TheFlatProject/gen/com/FourTheFlat/R.java
+++ 4TheFlatProject/gen/com/FourTheFlat/R.java
@@ -261,11 +261,12 @@ containing a value of this type.
         public static final int login=0x7f030001;
         public static final int main=0x7f030002;
         public static final int map=0x7f030003;
-        public static final int products=0x7f030004;
-        public static final int register=0x7f030005;
-        public static final int registered=0x7f030006;
-        public static final int settings=0x7f030007;
-        public static final int shoppinglist=0x7f030008;
+        public static final int password_change=0x7f030004;
+        public static final int products=0x7f030005;
+        public static final int register=0x7f030006;
+        public static final int registered=0x7f030007;
+        public static final int settings=0x7f030008;
+        public static final int shoppinglist=0x7f030009;
     }
     public static final class string {
         public static final int app_name=0x7f06001a;
