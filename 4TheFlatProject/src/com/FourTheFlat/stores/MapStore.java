efc2cc3348590e4ce1c026bbe3c584550b5b48e9
David Burrell <d_burrell7@hotmail.co.uk>
2014-03-23 19:52:58 +0000

    display and clear debt

---
+++ 4TheFlatProject/src/com/FourTheFlat/stores/MapStore.java
@@ -0,0 +1,18 @@
+package com.FourTheFlat.stores;
+
+import java.util.Map;
+
+public class MapStore {
+
+	Map<String, Integer> map;
+
+	public Map<String, Integer> getMap()
+	{
+		return map;
+	}
+
+	public void setMap(Map<String, Integer> map)
+	{
+		this.map = map;
+	}
+}
