1782c9636801d510089ed360b1f7a1a75a196bac
Alexander Grant <alexander.grant55@gmail.com>
2014-03-23 17:24:04 +0000

    Fixes for server side update

--- 4TheFlatProject/src/com/FourTheFlat/stores/Group.java
+++ 4TheFlatProject/src/com/FourTheFlat/stores/Group.java
@@ -1,6 +1,7 @@
 package com.FourTheFlat.stores;
 
 
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
@@ -10,69 +11,67 @@ public class Group {
 	Set<String> users;
 	private String address;
 	private Set<String> allowedProducts;
-	private Set<String> shoppingList;
-	private boolean userShopping;
-	
+	private Map<String, Integer> shoppingList;
+	private String userShopping;
+
 	public UUID getGroupID()
 	{
 		return groupID;
 	}
-	
+
 	public void setGroupID(UUID groupID)
 	{
 		this.groupID = groupID;
 	}
-	
+
 	public void setUsers(Set<String> users)
 	{
 		this.users = users;
 	}
-	
+
 	public Set<String> getUsers()
 	{
 		return users;
 	}
-		
-	
+
+
 	public String getAddress()
 	{
 	  return address;
 	}
-	
-	
+
+
 	public void setAddress(String address)
 	{
 		this.address = address;
 	}
-	
+
 	public void setAllowedProducts(Set<String> allowedProducts)
 	{
 		this.allowedProducts = allowedProducts;
 	}
-	
+
 	public Set<String> getAllowedProducts()
 	{
 		return allowedProducts;
 	}
-	
-	public void setShoppingList(Set<String> shoppingList)
-	{
-		this.shoppingList =shoppingList;
-	}
-	
-	public Set<String> getShoppingList()
-	{
-		return shoppingList;
-	}
 
-	
-	public void setUserShopping(boolean userShopping)
+
+	public void setUserShopping(String userShopping)
 	{
 		this.userShopping = userShopping;
 	}
-	
-	public boolean getuserShopping()
+
+	public String getuserShopping()
 	{
 		return userShopping;
+	}
+
+	public Map<String, Integer> getShoppingList() {
+		return shoppingList;
+	}
+
+	public void setShoppingList(Map<String, Integer> shoppingList) {
+		this.shoppingList = shoppingList;
 	}
 }
